6802a9a4b5ad77bff5700d0ce9d580bd2a7d529b
leticia0307 <leticia.aparecida.souza03@escola.pr.gov.br>
2026-03-18 15:10:18 -0300

    Update sketh.js

--- sketh.js
+++ sketh.js
@@ -1,11 +1,11 @@
-function setup(){
+function setup() {
     createCanvas(1920,1080, WEBGL);//plano 3D
-    terra=loadImagem('terra.jpg')
+    terra=loadImage('terra.jpg')
 }
 
-function draw(){
-    backgrond(205, 102, 94);//cor de fundo
-    rotateY(millis()/1000);//rotaçâo da terra
-    texture(terra);//texture da terra, imagem.
+function draw() {
+    background(205, 102, 94);//cor de fundo
+    rotateY(millis()/1000);//rotação da terra
+    texture(terra);//textura da terra, imagem.
     sphere(80, 100);//esfera 3D
- }
+}
